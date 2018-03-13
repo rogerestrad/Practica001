5e78d2fb605c39b603b504bb242a47a547bce443
Edgar Rogelio <rogerestrad@gmail.com>
2018-03-12 21:10:09 -0600

    Initial commit

---
+++ app/src/main/java/com/rogerestrad/practica001/MainActivity.java
@@ -0,0 +1,27 @@
+package com.rogerestrad.practica001;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText cuadro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    cuadro= (EditText)findViewById(R.id.TxtCadena);
+    }
+    public void verificar(View v){
+        String  invertida ="";
+        String palabra=cuadro.getText().toString();
+        for (int x=palabra.length()-1;x>=0;x--)
+        invertida = invertida + palabra.charAt(x);
+        cuadro.setText(invertida);
+
+    }
+    public void sumar (View v){
+
+    }
+}
